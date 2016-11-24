43988d493a36fe1d0c5fef81b73ea2ec36e6a383
eleanor keane <egjkeane@gmail.com>
2016-11-24 08:10:21 +0000

    removed api calls

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FCCopenWeather
 FCC Open Weather app by Samatar, Harry and E
+
+In order to save on API calls I have commented out the promise chain ( line 33 - 56 ) and replaced jsonObject with my own custom weather object (the weather for where I live). Feel free to change this custom object as you wish!!
+(this will break the functionality of the change-units button, since the units are applied in the API call, but it is working don't worry!)
--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+var jsonObject = {
+  "coord": {
+    "lon": -0.1,
+    "lat": 51.38
+  },
+  "weather": [
+    {
+      "id": 803,
+      "main": "Clouds",
+      "description": "broken clouds",
+      "icon": "04d"
+    }
+    ],
+      "base": "stations",
+      "main": {
+      "temp": 9.45,
+      "pressure": 1022,
+      "humidity": 87,
+      "temp_min": 8,
+      "temp_max": 11
+      },
+        "visibility": 9000,
+        "wind": {
+        "speed": 8.2,
+        "deg": 50
+        },
+          "clouds": {
+          "all": 75
+          },
+            "dt": 1479973800,
+            "sys": {
+            "type": 1,
+            "id": 5088,
+            "message": 0.0564,
+            "country": "GB",
+            "sunrise": 1479972830,
+            "sunset": 1480003220
+            },
+              "id": 2651817,
+              "name": "Croydon",
+              "cod": 200};
 var locationApiUrl = "http://geoip.nekudo.com/api";
 var baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
 var lat = "lat=";
@@ -5,7 +46,6 @@ var long = "&lon=";
 var apiId = "&appid=afe224f25cdf028a03b1b75949c0d9cc";
 var cityQuery = "q=";
 var cityName = "";
-var jsonObject;
 var isMetric = "&units=metric";
 var unitsButton = document.getElementById('changeUnits');
 
@@ -30,31 +70,32 @@ function getLeJSON(url) {
 }
 
 function startUp() {
-getLeJSON(locationApiUrl).then(
-function(response) {
-  // congrats you have a Blob with location data!
-  // next build a url for weather api using the data
-  var locationJson = JSON.parse(response);
-  console.log(response, "le response");
-  console.log(locationJson, "location json");
-  return baseUrl + lat + locationJson.location.latitude + long + locationJson.location.longitude + apiId + isMetric;
-  // after that we need to request the Blob from weather api (see next chained promise with .then)
-
-}, function(error){
-  // there was a problem with getting location so prompt for city name
-  cityName = prompt("There was a problem! Error:" + error + ". Please input your city manually.");
-   return baseUrl + cityQuery + cityName + apiId + isMetric;
-}
-).then(function(weatherUrl){ console.log(weatherUrl);
-                  var xhr = new XMLHttpRequest();
-                  xhr.open("GET", weatherUrl, false);
-                  xhr.send();
-                  console.log(xhr.status)
-                  console.log(xhr.responseText);
-                  jsonObject = JSON.parse (xhr.responseText);
-                  return jsonObject;
-             }).then( function(response) { console.log(response);
-                                         return viewInitialise(); });
+// getLeJSON(locationApiUrl).then(
+// function(response) {
+//   // congrats you have a Blob with location data!
+//   // next build a url for weather api using the data
+//   var locationJson = JSON.parse(response);
+//   console.log(response, "le response");
+//   console.log(locationJson, "location json");
+//   return baseUrl + lat + locationJson.location.latitude + long + locationJson.location.longitude + apiId + isMetric;
+//   // after that we need to request the Blob from weather api (see next chained promise with .then)
+//
+// }, function(error){
+//   // there was a problem with getting location so prompt for city name
+//   cityName = prompt("There was a problem! Error:" + error + ". Please input your city manually.");
+//    return baseUrl + cityQuery + cityName + apiId + isMetric;
+// }
+// ).then(function(weatherUrl){ console.log(weatherUrl);
+//                   var xhr = new XMLHttpRequest();
+//                   xhr.open("GET", weatherUrl, false);
+//                   xhr.send();
+//                   console.log(xhr.status)
+//                   console.log(xhr.responseText);
+//                   jsonObject = JSON.parse (xhr.responseText);
+//                   return jsonObject;
+//              }).then( function(response) { console.log(response);
+                                         return viewInitialise();
+                                      //  });
 }
 
 function viewInitialise() {
